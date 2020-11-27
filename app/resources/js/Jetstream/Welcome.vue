3f84bf6bc0146f9d3bb4a36fe19332a2cf349dbb
Jonatan Baumgartner <jonatan.baumgartner@he-arc.ch>
2020-11-27 15:17:20 +0100

    page accueil compte

--- app/resources/js/Jetstream/Welcome.vue
+++ app/resources/js/Jetstream/Welcome.vue
@@ -6,14 +6,12 @@
             </div>
 
             <div class="mt-8 text-2xl">
-                Welcome to your Jetstream application!
+                Bienvenue sur EZWorkout
             </div>
 
             <div class="mt-6 text-gray-500">
-                Laravel Jetstream provides a beautiful, robust starting point for your next Laravel application. Laravel is designed
-                to help you build your application using a development environment that is simple, powerful, and enjoyable. We believe
-                you should love expressing your creativity through programming, so we have spent time carefully crafting the Laravel
-                ecosystem to be a breath of fresh air. We hope you love it.
+                EZWorkout vous permet de planifier facilement vos entraînements et de voir vos résultats en un coup d'oeil.
+                Télécharger l'application android EZWorkout et connectez-la à votre compte afin de suivre vos entraînements.
             </div>
         </div>
 

e906c7f6edf29bab2e0af6a4dd02bcc119c77b6b
Jonatan Baumgartner <jonatan.baumgartner@he-arc.ch>
2020-11-27 15:44:59 +0100

    TrainingPlan ressource

--- app/app/Http/Controllers/TrainingPlanController.php
+++ app/app/Http/Controllers/TrainingPlanController.php
@@ -2,9 +2,77 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Training;
 use Illuminate\Http\Request;
 
 class TrainingPlanController extends Controller
 {
-    //
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return response()->json(Training::getAll());
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $data = $request->validate([
+            'name' => 'string'
+        ]);
+        $t = new Training();
+        $t->name = $data['name'];
+        $t->user_id = $request->user()->id;
+        $t->save();
+        return response()->json($t);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        return response()->json(Training::find($id));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $data = $request->validate([
+            'name' => 'string'
+        ]);
+        $t = Training::find($id);
+        $t->name = $data['name'];
+        $t->save();
+        return response()->json($t);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $t = Training::destroy($id);
+        return response()->json(['delete' => 'ok']);
+    }
 }

def1568325fb41a7db4941a6c42576063fea27f7
longPointyEyes <m-assi12@hotmail.fr>
2020-12-04 11:21:11 +0100

    todo comment

--- app/resources/js/Pages/SelectResults.vue
+++ app/resources/js/Pages/SelectResults.vue
@@ -35,7 +35,7 @@
                                 </tr>
                             </thead>
                             <tbody class="bg-white divide-y divide-gray-200">
-                                <tr>
+                                <tr> <!-- TODO: v-for here -->
                                 <td class="px-6 py-4 whitespace-nowrap">
                                     <div class="flex items-center">
                                     <div class="ml-4">
@@ -84,5 +84,7 @@
             AppLayout,
             Welcome,
         },
+
+        
     }
 </script>

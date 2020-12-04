57c229560d9769fa5b4db079389db26271a43c0c
Jonatan Baumgartner <jonatan.baumgartner@he-arc.ch>
2020-12-04 09:05:23 +0100

    seeder partie haute de db

--- app/database/seeders/DatabaseSeeder.php
+++ app/database/seeders/DatabaseSeeder.php
@@ -2,6 +2,9 @@
 
 namespace Database\Seeders;
 
+use App\Models\Exercise;
+use App\Models\TrainingPlan;
+use App\Models\Training;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -13,10 +16,59 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        //training plan
+        $tp = new TrainingPlan([
+            'name' => 'Cycle de force',
+            'user_id' => 1
+        ]);
+        $tp->save();
 
-        \App\Models\Exercise::factory(10)->create();
+        $tp->find(1)->trainings()->saveMany([
+            new Training(['name' => 'Pec - triceps']),
+            new Training(['name' => 'Dos - Biceps'])
+        ]);
 
+        $trainings = Training::all();
+        $trainings[0]->exercises()->saveMany([
+            new Exercise([
+                'name' => 'Developpé couché',
+                'comment' => 'On s\'en fout mec',
+                'nbSerie' => 1,
+                'repMin' => 8,
+                'repMax' => 12,
+                'pauseSerie' => 60,
+                'pauseExercise' => 120
+            ]),
+            new Exercise([
+                'name' => 'Kickback',
+                'comment' => 'On s\'en fout',
+                'nbSerie' => 1,
+                'repMin' => 8,
+                'repMax' => 12,
+                'pauseSerie' => 60,
+                'pauseExercise' => 120
+            ]),
+        ]);
+        $trainings[1]->exercises()->saveMany([
+            new Exercise([
+                'name' => 'Tirage vertical',
+                'comment' => 'OSEF',
+                'nbSerie' => 1,
+                'repMin' => 8,
+                'repMax' => 12,
+                'pauseSerie' => 60,
+                'pauseExercise' => 120
+            ]),
+            new Exercise([
+                'name' => 'Curl',
+                'comment' => 'On s\'en fout',
+                'nbSerie' => 1,
+                'repMin' => 8,
+                'repMax' => 12,
+                'pauseSerie' => 60,
+                'pauseExercise' => 120
+            ]),
+        ]);
     }
     
 }

43b2fc0aae57832e4bdef2faac19b0deb0f25257
Jonatan Baumgartner <jonatan.baumgartner@he-arc.ch>
2020-12-05 14:07:54 +0100

    addind trainingPlanPage

---
+++ app/resources/js/Pages/TrainingPlan.vue
@@ -0,0 +1,107 @@
+<template>
+    <div>
+        <app-layout>
+        <template #header>
+            <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+                Vos plans d'entraînement
+            </h2>
+        </template>
+
+        <div class="py-12">
+            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+                <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
+                    <div class="flex flex-col">
+                    <div class="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
+                        <div class="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
+                        <div class="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg">
+                            <table class="min-w-full divide-y divide-gray-200">
+                            <thead>
+                                <tr>
+                                <th scope="col" class="px-6 py-3 bg-gray-50 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                                    Name
+                                </th>
+                                <th scope="col" class="px-6 py-3 bg-gray-50 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                                    Title
+                                </th>
+                                <th scope="col" class="px-6 py-3 bg-gray-50 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                                    Status
+                                </th>
+                                <th scope="col" class="px-6 py-3 bg-gray-50 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                                    Role
+                                </th>
+                                <th scope="col" class="px-6 py-3 bg-gray-50">
+                                    <span class="sr-only">Edit</span>
+                                </th>
+                                </tr>
+                            </thead>
+                            <tbody class="bg-white divide-y divide-gray-200">
+                                <tr v-for="plan in training_plans" :key="plan.id"> <!-- TODO: v-for here -->
+                                <td class="px-6 py-4 whitespace-nowrap">
+                                    <div class="flex items-center">
+                                    <div class="ml-4">
+                                        <div class="text-sm font-medium text-gray-900">
+                                        {{plan.name}}
+                                        </div>
+                                    </div>
+                                    </div>
+                                </td>
+                                <td class="px-6 py-4 whitespace-nowrap">
+                                    <div class="text-sm text-gray-900">Regional Paradigm Technician</div>
+                                    <div class="text-sm text-gray-500">Optimization</div>
+                                </td>
+                                <td class="px-6 py-4 whitespace-nowrap">
+                                    <span class="px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-green-100 text-green-800">
+                                    Active
+                                    </span>
+                                </td>
+                                <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
+                                    Admin
+                                </td>
+                                <td class="px-6 py-4 whitespace-nowrap text-right text-sm font-medium">
+                                    <a href="#" class="text-indigo-600 hover:text-indigo-900">Edit</a>
+                                </td>
+                                </tr>
+                            </tbody>
+                            </table>
+                        </div>
+                        </div>
+                    </div>
+                    </div>
+                    
+                </div>
+            </div>
+        </div>
+    </app-layout>
+    </div>
+</template>
+
+<script>
+    import AppLayout from './../Layouts/AppLayout'
+    import Welcome from './../Jetstream/Welcome'
+
+    export default {
+        components: {
+            AppLayout,
+            Welcome,
+        },
+
+        data(){
+            return{
+                training_plans:[],
+            }
+        },
+        methods:{
+            getTrainingPlans(){
+                axios.get('/trainingPlan')
+                .then((res) => {
+                    this.training_plans = res.data
+                }).catch((err) => {
+                    console.log(err)
+                });
+            },
+        },
+        created(){
+            this.getTrainingPlans()
+        }
+    }
+</script>

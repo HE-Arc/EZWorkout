ecdc303e9c058f02ca17fed54b926c947cdb65f3
ma2s <massimo.desantis@he-arc.ch>
2020-12-11 20:08:13 +0100

    reshape data table

--- app/resources/js/Pages/Results.vue
+++ app/resources/js/Pages/Results.vue
@@ -6,7 +6,7 @@
                 Results ?
             </h2>
             {{training_plans_effective.logbook_pages[0].training_effs[0].exercise_effs[0].series_effs[0].rep}}
-            {{training_plans_template.training}}
+            {{training_plans_template.trainings[0].exercises[0].name}}
         </template>
         <div class="py-12">
             <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
@@ -16,36 +16,22 @@
                         <div class="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
                         <div class="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg">
                             <table  class="border-collapse border border-gray-200">
-                                  <thead>
-                                      <tr class="border border-collapse border-gray-400 ..."> <!-- TODO: v-for here -->
-                                          <th>Exercice</th>
-                                          <th colspan="2" class="border border-collapse border-gray-200 ...">training1</th> <!-- TODO: colspan value from nb of series -->
-                                          <th colspan="2" class="border border-collapse border-gray-200 ...">training2</th>
-                                          <th colspan="2" class="border border-collapse border-gray-200 ...">training3</th>
-                                     </tr>
-                                    <div v-for="training in training_plans_template.trainings" :key="training.id">
-                                        <tr class="border border-collapse border-gray-400 ...">
-                                            <div v-for="exo in training.exercises" :key="exo.id" >
-                                                <th class="border border-collapse border-gray-200 ..." >{{exo.name}}</th>
-                                             </div>
-                                        </tr>
-                                      </div>
-                                </thead>
-                                <tbody>
-                                    <div v-for="page in training_plans_effective.logbook_pages" :key="page.id">
-                                        <tr v-for="training in page.training_effs" :key="training.id">
-                                        <td v-for="exo in training.exercise_effs" :key="exo.id" class="border border-gray-200 ...">{{exo.rating}}</td>
-                                        </tr>
-                                        <tr>
-                                        <td class="border border-gray-200 ...">Ohio</td>
-                                        <td class="border border-gray-200 ...">Columbus</td>
-                                        </tr>
-                                        <tr>
-                                        <td class="border border-gray-200 ...">Michigan</td>
-                                        <td class="border border-gray-200 ...">Detroit</td>
+                                 <div v-for="(page,index) in training_plans_effective.logbook_pages" :key="page.id" >
+                                    <tr class="border border-collapse border-gray-400 ">
+                                        <th class="border border-collapse border-gray-200 ">Exercice</th>
+                                        <th class="border border-collapse border-gray-200 ">Seance {{index+1}}</th> <!-- TODO: v-for here -->
+                                    </tr>
+                                 <div v-for="training in page.training_effs" :key="training.id" class="border border-collapse border-gray-400">
+                                    <tr v-for="exo in training.exercise_effs" :key="exo.id" class="border border-collapse border-gray-200">
+                                            <th class="border border-collapse border-gray-200 ..." >{{exo.id}}</th>
+                                            <td v-for="serie in exo.series_effs" :key="serie.id">{{serie.rep}} </td>
+                                    </tr>
+                                </div>
+                                   
+                                        <tr >
+                                        <td class="border border-gray-200"></td>
                                         </tr>
                                     </div>
-                                </tbody>
                             </table>
                         </div>
                         </div>

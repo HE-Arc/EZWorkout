94ae5a61f5561fb32f32daeba59e66e0efb3bae4
ma2s <massimo.desantis@he-arc.ch>
2020-12-11 18:35:58 +0100

    constructing table

--- app/resources/js/Pages/Results.vue
+++ app/resources/js/Pages/Results.vue
@@ -6,6 +6,7 @@
                 Results ?
             </h2>
             {{training_plans_effective.logbook_pages[0].training_effs[0].exercise_effs[0].series_effs[0].rep}}
+            {{training_plans_template.training}}
         </template>
         <div class="py-12">
             <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
@@ -16,14 +17,24 @@
                         <div class="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg">
                             <table  class="border-collapse border border-gray-200">
                                   <thead>
-                                    <tr v-for="page in training_plans_effective.logbook_pages" :key="page.id">
-                                    <th class="border border-gray-200 ..." :colspan = "page.training_effs[0].exercise_effs[0].series_effs.length">Page {{page.id}}</th><!--TOFIX: display number and series number (keep only the max of series of the plan)  -->
-                                    </tr>
+                                      <tr class="border border-collapse border-gray-400 ..."> <!-- TODO: v-for here -->
+                                          <th>Exercice</th>
+                                          <th colspan="2" class="border border-collapse border-gray-200 ...">training1</th> <!-- TODO: colspan value from nb of series -->
+                                          <th colspan="2" class="border border-collapse border-gray-200 ...">training2</th>
+                                          <th colspan="2" class="border border-collapse border-gray-200 ...">training3</th>
+                                     </tr>
+                                    <div v-for="training in training_plans_template.trainings" :key="training.id">
+                                        <tr class="border border-collapse border-gray-400 ...">
+                                            <div v-for="exo in training.exercises" :key="exo.id" >
+                                                <th class="border border-collapse border-gray-200 ..." >{{exo.name}}</th>
+                                             </div>
+                                        </tr>
+                                      </div>
                                 </thead>
                                 <tbody>
-                                    <div v-for="training in training_plans_effective.logbook_pages[0].training_effs" :key="training.id">
-                                        <tr v-for="exo in training.exercise_effs" :key="exo.id">
-                                        <td class="border border-gray-200 ..."><b>{{exo.rating}}</b></td>
+                                    <div v-for="page in training_plans_effective.logbook_pages" :key="page.id">
+                                        <tr v-for="training in page.training_effs" :key="training.id">
+                                        <td v-for="exo in training.exercise_effs" :key="exo.id" class="border border-gray-200 ...">{{exo.rating}}</td>
                                         </tr>
                                         <tr>
                                         <td class="border border-gray-200 ...">Ohio</td>
